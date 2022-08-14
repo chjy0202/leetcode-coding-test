88474ede326206ac15df577e55d849d29a2a172a
Hyunjung Choi <72859045+chjy0202@users.noreply.github.com>
2022-08-14 17:31:46 +0900

    Time: 85 ms (57.27%), Space: 42 MB (61.16%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -4,9 +4,9 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    let needleLn = needle.length;
+    // let needleLn = needle.length;
     for (let i = 0; i < haystack.length; i++) {
-        if (haystack.slice(i, i + needleLn) === needle) {
+        if (haystack.slice(i, i + needle.length) === needle) {
             return i;
         }
     }
